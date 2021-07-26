2ff247015d7b7f0be600671964ebcd5836d4e8c0
Lu-a-png <70081198+Lu-a-png@users.noreply.github.com>
2021-07-26 07:29:05 -0300

    Update Main.php

--- src/Luz/Main.php
+++ src/Luz/Main.php
@@ -36,7 +36,8 @@ class Main extends Wolf {
             switch($args[0]){
               
               case "on":
-                $sender->addEffect(16);
+                $this->getPlayer()->getEffect((16), INT32_MAX, 1, false);
+                //$effect = new EffectInstance(Effect::getEffect(16), INT32_MAX, 1, false);
                 $sender->sendMessage("§7[§6LUZ§7] §aLuz Ativada Com Sucesso !");
 
                 return true;
